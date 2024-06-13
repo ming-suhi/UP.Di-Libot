e10600081efff08ba12ed30564f3a262b6d3bce6
ming-suhi <ming-suhi@users.noreply.github.com>
2024-06-13 12:21:48 +0000

    deploy: eb14ead54ddf6e91dda6d7b1a1a6e2e839302e73

---
+++ worker.js
@@ -0,0 +1,19 @@
+"use strict";
+// @ts-nocheck
+// Issue with using Turf typings
+importScripts('https://cdn.jsdelivr.net/npm/@turf/turf@6/turf.min.js');
+self.onmessage = function (event) {
+    const { points, BOUNDARY } = event.data;
+    const T = turf;
+    let turfPolygon = T.polygon([[
+            [-180, -90], [180, -90], [180, 90], [-180, 90], [-180, -90]
+        ]]);
+    points.forEach(point => {
+        const turfPoint = T.point([point.longitude, point.latitude]);
+        if (T.booleanPointInPolygon(turfPoint, BOUNDARY)) {
+            const buffer = T.buffer(turfPoint, 100, { units: 'meters' });
+            turfPolygon = T.difference(turfPolygon, buffer);
+        }
+    });
+    self.postMessage(turfPolygon);
+};
